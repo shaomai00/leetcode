36bf00be9d5b34486a878d5b26950d76069bc25c
魏梦溪 <it@ITdeMacBook-Pro.local>
2022-12-06 16:39:18 +0800

    20221206

---
+++ 1.两数之和.py
@@ -0,0 +1,21 @@
+from typing import List
+# 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那两个整数，并返回它们的数组下标。
+# 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+# 你可以按任意顺序返回答案。
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # 创建一个字典 存储{num: index}
+        d = {}
+        # 遍历数组中的每个数字
+        for i,num in enumerate(nums):
+            # 如果目标值减去当前值在字典中，说明找到了这两个数字，返回相应的下表即可
+            if target - num in d:
+                return [i, d[target-num]]
+            # 将当前数字的下标存进字典
+            d[num] = i
+        return [-1,-1]
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.twoSum(nums = [2,7,11,15], target = 9))
--- 292_NimGame.java
+++
@@ -1,8 +0,0 @@
-public class Solution {
-    public boolean canWinNim(int n) {
-        if(n%4!=0){
-            return true;
-        }else
-        return false;
-    }
-}
